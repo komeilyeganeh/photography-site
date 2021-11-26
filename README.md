009118e5e80f886d12fef4b3bfde5e2c794e2a1e
komeilyeganeh <komeilyeganeh261@gmail.com>
2021-11-26 14:01:54 +0330

    Fix bug menu - complete the album page

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Photography Site
 
 - Home page
+- Fix bug menu
+- Album page
 
 
 ## Skills : Html Css Javascript
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* script slider - swiper js */
 var swiper = new Swiper(".swiper", {
-    loop: true,
+  loop: true,
   autoplay: {
     delay: 4500,
     disableOnInteraction: false,
@@ -12,13 +12,24 @@ var swiper = new Swiper(".swiper", {
   },
 });
 
-
 /* open/close menu mobile */
 let bars = document.querySelector(".header .navbar i.fa-bars");
 let times = document.querySelector(".navbar-mobile .navbar i.fa-times");
 bars.onclick = () => {
-    document.querySelector(".navbar-mobile").classList.toggle("active");
-}
+  let navMobile = document.querySelector(".navbar-mobile");
+  navMobile.classList.toggle("active");
+  if (navMobile.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  }
+};
 times.onclick = () => {
-    document.querySelector(".navbar-mobile").classList.remove("active");
-}
+  document.querySelector(".navbar-mobile").classList.remove("active");
+  document.body.style.overflow = "auto";
+};
+
+let el = document.querySelectorAll(".navbar-mobile ul li");
+el.forEach((e) => {
+  e.onclick = () => {
+    e.querySelector("ul.submenu").classList.toggle("active");
+  };
+});
